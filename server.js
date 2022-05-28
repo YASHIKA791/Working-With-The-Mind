46739a06f98eeaf290d0645dbb86511b2d885cdc
Yashika <mails.yashika@gmail.com>
2022-05-28 20:18:38 +0530

    port updated

--- server.js
+++ server.js
@@ -196,6 +196,10 @@ server.post("/reset", function (req, res) {
   });
 
 });
-server.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+server.listen(port, function () {
   console.log("Server started on port 3000");
 });
